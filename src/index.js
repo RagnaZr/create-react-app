070e659545c628df7a2adc73368184bba9f2f19d
thanhv2 <trieuvuong0168.com>
2019-06-12 23:21:54 +0700

    i

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDom from "react-dom";
-import App from './App';
+import App from './app';
 
 const contentElement = document.getElementById("content");
 ReactDom.render(<App />,contentElement)
