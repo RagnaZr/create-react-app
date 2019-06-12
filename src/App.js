c141c4e6d0ced3bacade9f58ea0ab4cf2f993a2d
thanhv2 <trieuvuong0168.com>
2019-06-12 21:59:21 +0700

    i

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import LoginPage from './components/LoginPage/Login';
+import SignupPage from './components/SignupPage/Signup';
 
+
+const Signup = () => (
+     <SignupPage text = "Please fill in this form to create an account." />
+   );
+   
+const Login = () => (
+     <LoginPage />
+);
+   
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-
+      render()  {
+            return  (
+        <Router>
+          <div>
+            <ul>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/signup">Signup</Link></li>
+            </ul>
+            <Route path="/login" component={Login}/>
+            <Route path="/signup" component={Signup}/>
+          </div>
+        </Router>
+            );
+          }
+         }
 export default App;
