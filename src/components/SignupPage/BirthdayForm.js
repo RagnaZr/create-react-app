bb5ffe0223d385f46551168829542abc9f3c51e0
RagnaZr <47023623+RagnaZr@users.noreply.github.com>
2019-06-13 23:33:58 +0700

    Update BirthdayForm.js

--- src/components/SignupPage/BirthdayForm.js
+++ src/components/SignupPage/BirthdayForm.js
@@ -1,36 +1,29 @@
-import React from "react";
-import DatePicker from "react-datepicker";
-import addDays from "date-fns/addDays"
-import "react-datepicker/dist/react-datepicker.css";
+import React from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
 
-
+import 'react-datepicker/dist/react-datepicker.css';
 
 class BirthdayForm extends React.Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        startDate: new Date()
-      };
-      this.handleChange = this.handleChange.bind(this);
+        super(props)
+        this.state = {
+            startDate: moment()
+        };
+        this.handleChange = this.handleChange.bind(this);
     }
-   
-    handleChange(date) {
-      this.setState({
-        startDate: date
-      });
-    }
-   
-    render() {
-      return (
-        <DatePicker
-           selected={this.state.startDate}
-           onChange={this.handleChange}
-           // Khong chon ngay tuong lai
-           maxDate={addDays(new Date(), 0)}
-           placeholderText="Click to select a date" 
-        />
-      );
-    }
-  }
 
+    handleChange(date) {
+        this.setState({
+            startDate: date
+        });
+    }
+
+    render() {
+        return <DatePicker
+            selected={this.state.startDate}
+            onChange={this.handleChange}
+        />;
+    }
+}
 export default BirthdayForm;
